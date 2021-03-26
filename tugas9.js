112e594cd7308a5e02edcc8a062cf9e7ad9a97c0
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-03-26 14:06:04 +0700

    Tugas 9 Javascript
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ tugas9.js
@@ -0,0 +1,8 @@
+let masakanNusantara ={
+  bagianKiri:"pendap, rendang, pempek",
+  bagianTengah:"gudeg, serabi, rawon",
+  bagianKanan:"Sup Ikan Jantung Pisang, Ayam Taliwang, papeda"
+}
+for(var x in masakanNusantara){
+  console.log(masakanNusantara[x]);
+}
